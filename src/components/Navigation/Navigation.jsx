1ce208ccf071be6b1599deabfc62df1b0366c1cc
NataliiaPleskanka <154823178+NataliiaPleskanka@users.noreply.github.com>
2024-10-02 17:20:31 +0200

    nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+import css from "./Navigation.module.css";
+import clsx from "clsx";
 
-const Navigation = () => {
-  return <div>Navigation</div>;
-};
+function Navigation() {
+  const buildNavLinkClass = ({ isActive }) => {
+    return clsx(css.link, isActive && css.active);
+  };
+
+  return (
+    <div className={css.navContainer}>
+      <nav className={css.nav}>
+        <NavLink to="/" className={buildNavLinkClass} end>
+          Home
+        </NavLink>
+
+        <NavLink to="/movies" className={buildNavLinkClass}>
+          Movies
+        </NavLink>
+      </nav>
+    </div>
+  );
+}
 
 export default Navigation;
