3218cb0b13640d6f55bd001305c4dc79d4f936d7
NataliiaPleskanka <154823178+NataliiaPleskanka@users.noreply.github.com>
2024-10-03 15:51:47 +0200

    axios-js

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import axios from "axios";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+axios.defaults.baseURL = "https://api.themoviedb.org/3/";
+
+const options = {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNWJkZDBjMzYwYTllNWIzYjlkMmI0MjFhYTk2MDNmMSIsIm5iZiI6MTcyNzg5NzM2Ny4zMjU4OTUsInN1YiI6IjY2ZjUyYTE4MmMzNjFlOTI1MWVkM2UzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rVaSSwgslwuKL8zdGVycjDPq9A8GVo2JKeSSqQS0f6g",
+  },
 };
 
-export default HomePage;
+export const fetchMovies = async () => {
+  const { data } = await axios.get(
+    "trending/movie/day?language=en-US",
+    options
+  );
+  return data.results;
+};
+
+export const fetchSearchMovie = async (query) => {
+  const { data } = await axios.get(
+    `search/movie?query=${query}&page=1`,
+    options
+  );
+  return data.results;
+};
+
+export const fetchMovieDetails = async (movieId) => {
+  const { data } = await axios.get(`movie/${movieId}`, options);
+  return data;
+};
+
+export const fetchCast = async (movieId) => {
+  const { data } = await axios.get(`movie/${movieId}/credits`, options);
+  return data.cast;
+};
+
+export const fetchReviews = async (movieId) => {
+  const { data } = await axios.get(`movie/${movieId}/reviews`, options);
+  return data.results;
+};
